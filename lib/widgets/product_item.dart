ee57faa7214c9f14d2223be83a159b0bb9a0b597
vnikolic7821rn <vnikolic7821rn@raf.rs>
2023-02-25 01:43:39 +0100

    Added sample data and created GridTile

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        this.product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          this.product.title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}

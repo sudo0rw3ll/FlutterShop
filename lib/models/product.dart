3ee4f1d344dd70205819ed347aa5b8e6ea15de09
vnikolic7821rn <vnikolic7821rn@raf.rs>
2023-02-25 01:12:14 +0100

    Added model and screens

---
+++ lib/models/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final double price;
+  bool isFavorite;
+
+  Product(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.imageUrl,
+      required this.price,
+      this.isFavorite = false});
+}
